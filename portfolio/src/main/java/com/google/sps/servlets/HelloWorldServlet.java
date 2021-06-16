94fb6f2abec049867dc60cfa964b581e551cfb69
Nicole <nicoleicasco@gmail.com>
2021-06-16 00:17:19 +0000

    Cleaned up white spaces, deleted unnecessary and text.

--- portfolio/src/main/java/com/google/sps/servlets/HelloWorldServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/HelloWorldServlet.java
@@ -17,12 +17,11 @@ public final class HelloWorldServlet extends HttpServlet {
   public void doGet(final HttpServletRequest request, final HttpServletResponse response) throws IOException {
     ArrayList<String> movies = new ArrayList<String>();
     movies.add("You've Got Mail");
+    movies.add("Miss Congeniality");
     movies.add("My Big Fat Greek Wedding");
-    movies.add("Monster's University");
 
-        // Convert the server stats to JSON
-    //HelloWorld helloWorld = new HelloWorld(movies);
-    String json = convertToJson(movies);
+   // Convert the movie list to JSON
+   String json = convertToJson(movies);
 
     // Send the JSON as the response
     response.setContentType("application/json;");
@@ -30,31 +29,19 @@ public final class HelloWorldServlet extends HttpServlet {
   }
 
   /**
-   * Converts a ServerMain instance into a JSON string using manual String concatentation.
+   * Converts a list of movies instance into a JSON string using manual String concatentation.
    */
   private String convertToJson(ArrayList<String> movies) {
     String json = "{";
-    json += "\"Movie #1\": ";
-    json += "\"" + movies.get(0) + "\"";
+    json += "\"Movie #1:" + movies.get(0) + "\"";
     json += ", ";
-    json += "\"Movie #2\": ";
-    json += "\"" + movies.get(1) + "\"";
+    json += "\"Movie #2:" + movies.get(1) + "\"";
     json += ", ";
-    json += "\"Movie #3\": ";
-    json += movies.get(2);
+    json += "\"Movie #3:" + movies.get(2);
     json += "}";
     return json;
   }
 
-  /**
-   * Converts a ServerStats instance into a JSON string using the Gson library. Note: We first added
-   * the Gson library dependency to pom.xml.
-   */
-  private String convertToJsonUsingGson(ArrayList<String> movies) {
-    Gson gson = new Gson();
-    String json = gson.toJson(movies);
-    return json;
-  }
 }
 
 
